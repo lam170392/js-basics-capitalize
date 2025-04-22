aaddbd559031bef2c921983d73eb53a986720957
lam170392 <lam170392@gmail.com>
2025-04-22 16:24:05 +0300

    feat: добавлена проверка на тип строки в capitalize

--- capitalize.js
+++ capitalize.js
@@ -1,7 +1,7 @@
-"use strict";
 function capitalize(str) {
+  if (typeof str !== "string") {
+    throw new TypeError("Input must be a string");
+  }
   if (!str) return "";
   return str[0].toUpperCase() + str.slice(1);
 }
-
-module.exports = capitalize;
--- capitalize.test.js
+++ capitalize.test.js
@@ -1,8 +1,11 @@
 // test.js
 const capitalize = require("./capitalize");
 
-// Ручные тесты:
 console.log(capitalize("hello") === "Hello"); // true
 console.log(capitalize("javaScript") === "JavaScript"); // true
 console.log(capitalize("") === ""); // true
+console.log(capitalize(" ") === " "); // true
+console.log(capitalize(" hello") === " hello"); // false
+console.log(capitalize("123abc") === "123abc"); // false
+console.log(capitalize("!abc") === "!abc"); // false
 console.log(capitalize("a") === "A"); // true
