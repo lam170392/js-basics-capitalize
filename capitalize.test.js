cf2fe364063417cb36495c640753f402f7347e92
lam170392 <lam170392@gmail.com>
2025-04-22 15:51:33 +0300

    feat: capitalize function

---
+++ capitalize.test.js
@@ -0,0 +1,8 @@
+// test.js
+const capitalize = require("./capitalize");
+
+// Ручные тесты:
+console.log(capitalize("hello") === "Hello"); // true
+console.log(capitalize("javaScript") === "JavaScript"); // true
+console.log(capitalize("") === ""); // true
+console.log(capitalize("a") === "A"); // true
