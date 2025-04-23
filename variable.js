0f02b1173779d3738644a8d2137f7ad0635b9e00
lam170392 <lam170392@gmail.com>
2025-04-23 17:25:17 +0300

    feat: добавлены переменные с базовыми типами данных

---
+++ variable.js
@@ -0,0 +1,14 @@
+// Имя (строка)
+const userName = "Алексей";
+
+// Возраст (число)
+let userAge = 33;
+
+// Есть ли опыт в программировании (булево)
+let hasExperience = false;
+
+// Текущий язык (null, потом мы его зададим)
+let currentLanguage = null;
+
+// Результат теста (undefined, ещё не вычислен)
+let testResult;
