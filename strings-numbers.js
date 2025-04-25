dae11d8d8db08044d0cd58a69eacfb437e12eec8
lam170392 <lam170392@gmail.com>
2025-04-25 16:56:10 +0300

    feat: реализована работа со строками и числами

---
+++ strings-numbers.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const name = "Алексей";
+const age = 33;
+let strNumber = "42";
+let balance = 1500;
+
+console.log(`Привет, меня зовут ${name}, мне ${age} года.`);
+console.log(` ${Number(strNumber) + 8}`);
+console.log(`На счету: ${balance.toString()}`);
