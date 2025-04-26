52b6e1e1554d5e7433a6782e4c17bed6b895a06e
lam170392 <lam170392@gmail.com>
2025-04-26 14:34:35 +0300

    fix: исправлена функция проверки входного значения

--- strings-numbers.js
+++ strings-numbers.js
@@ -8,3 +8,20 @@ let balance = 1500;
 console.log(`Привет, меня зовут ${name}, мне ${age} года.`);
 console.log(` ${Number(strNumber) + 8}`);
 console.log(`На счету: ${balance.toString()}`);
+
+function conversion(input) {
+  if (typeof input === "number") return "Это число";
+
+  if (typeof input === "string") {
+    // Проверяем, можно ли строку преобразовать в число
+    if (!isNaN(Number(input))) {
+      return "Это строка-число";
+    } else {
+      return "Это строка";
+    }
+  }
+
+  return "Неизвестный тип";
+}
+
+console.log(isNaN(Number("22")));
